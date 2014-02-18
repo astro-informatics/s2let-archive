0a9f720e18d643505591f89a718b80fbf3ec029b
Martin Büttner <m.buettner.d@gmail.com>
2014-02-18 18:13:42 +0000

    Add matlab function to computed rotated scaling function

--- src/main/matlab/s2let_compute_scal.m
+++ src/main/matlab/s2let_compute_scal.m
@@ -1,18 +1,19 @@
-function psi_j = s2let_compute_wav(j, alpha, beta, gamma, L, varargin)
-% s2let_compute_wav - Compute a rotated wavelet
+function phi_j = s2let_compute_scal(J_min, alpha, beta, gamma, L, varargin)
+% s2let_compute_scal - Compute a rotated scaling function
 %
-% Compute the j-th wavelet, rotated by rho=(alpha, beta, gamma) in
+% Compute the scaling function, rotated by rho=(alpha, beta, gamma) in
 % harmonic space and reconstruct it on the sphere.
 %
 % Default usage :
 %
-%   psi_j = s2let_compute_wav(j, alpha, beta, gamma, L, <options>)
+%   phi_j = s2let_compute_scal(J_min, alpha, beta, gamma, L, <options>)
 %
-% j is the order of the wavelet under consideration (depends on B)
+% J_min is the 1st wavelet scale of the tiling (and determines at which el
+% the scaling function stops).
 % rho=(alpha, beta, gamma) is the rotation in SO(3) by which to rotate
 % the wavelet wavelet
 % L if harmonic band-limit for the reconstruction on the sphere
-% psi_j is the reconstructed wavelet on the sphere, at resolution L
+% phi_j is the reconstructed wavelet on the sphere, at resolution L
 %
 % Options consist of parameter type and value pairs.
 % Valid options include:
@@ -27,7 +28,7 @@ function psi_j = s2let_compute_wav(j, alpha, beta, gamma, L, varargin)
 
 % Parse arguments.
 p = inputParser;
-p.addRequired('j', @isnumeric);
+p.addRequired('J_min', @isnumeric);
 p.addRequired('alpha', @isnumeric);
 p.addRequired('beta', @isnumeric);
 p.addRequired('gamma', @isnumeric);
@@ -36,7 +37,7 @@ p.addParamValue('B', 2, @isnumeric);
 p.addParamValue('N', -1, @isnumeric);
 p.addParamValue('Spin', 0, @isnumeric);
 
-p.parse(j, alpha, beta, gamma, L, varargin{:});
+p.parse(J_min, alpha, beta, gamma, L, varargin{:});
 
 args = p.Results;
 
@@ -48,7 +49,7 @@ B = args.B;
 N = args.N;
 Spin = args.Spin;
 
-psi = s2let_wavelet_tiling(B, L, N, Spin, j);
+[psi, phi] = s2let_wavelet_tiling(B, L, N, Spin, J_min);
 
 % Precompute Wigner small-d functions
 d = zeros(L, 2*L-1, 2*L-1);
@@ -58,8 +59,10 @@ for el = 1:L-1
 end
 
 % Rotate spherical harmonic
-psi_lm_rot = ssht_rotate_flm(psi(:,j+1), d, alpha, gamma);
+phi_lm_rot = ssht_rotate_flm(phi, d, alpha, gamma, 'Axisymmetric', true);
 
-psi_j = ssht_inverse(complex(real(psi_lm_rot), imag(psi_lm_rot)), L, 'Spin', Spin);
+
+
+phi_j = ssht_inverse(complex(real(phi_lm_rot), imag(phi_lm_rot)), L, 'Spin', Spin);
 
 end
