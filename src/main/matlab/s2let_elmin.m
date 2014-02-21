782472d0fc0bf9071b80e6d447e931bf6bbf928f
Martin Büttner <m.buettner.d@gmail.com>
2014-02-21 14:16:50 +0000

    Add matlab function to determine el_min

---
+++ src/main/matlab/s2let_elmin.m
@@ -0,0 +1,19 @@
+function elmin = s2let_elmin(B, j)
+
+% s2let_elmin
+% Return the lowest harmonic index el supported by the given
+% wavelet scale.
+%
+% Default usage:
+%		elmin = s2let_elmin(B, j)
+%
+%   B is the wavelet parameter,
+%   j the scale of interest.
+%
+% S2LET package to perform Wavelets transform on the Sphere.
+% Copyright (C) 2012  Boris Leistedt & Jason McEwen
+% See LICENSE.txt for license details
+
+elmin = ceil(B^j);
+
+end
