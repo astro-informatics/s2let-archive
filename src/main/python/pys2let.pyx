9efa6783a78cf1bdc7d2f6fcd9a8fc2108f1c56b
Boris Leistedt <boris.leistedt@gmail.com>
2015-02-11 01:09:46 +0000

    Added synthesis to wrappers

--- src/main/python/pys2let.pyx
+++ src/main/python/pys2let.pyx
@@ -8,7 +8,6 @@ cimport numpy as np
 # (not strictly necessary for this example)
 np.import_array()
 
-
 cdef extern from "s2let.h":
 
 	void s2let_transform_axisym_lm_allocate_wav(
@@ -56,7 +55,7 @@ def healpy_lm(el, em, L):
 	return em*(2*L-1-em)/2+el
 
 def pys2let_transform_axisym_lm_wav_analysis(
-	np.ndarray[double complex, ndim=1, mode="c"] flm_real not None, B, L, J_min):
+	np.ndarray[double complex, ndim=1, mode="c"] flm_hp not None, B, L, J_min):
 
 	cdef s2let_parameters_t parameters = {};
 	parameters.B = B;
@@ -72,13 +71,10 @@ def pys2let_transform_axisym_lm_wav_analysis(
 	f_scal_lm = np.empty([L * L,], dtype=complex)
 	f_wav_lm = np.empty([L * L * (J+1-J_min),], dtype=complex)
 
-	print flm_real.shape[0], f_lm.shape[0]
-
 	for el from 0 <= el < L:
 		for em from 0 <= em <= el:
-			#print el, em, el * (el + 1)/2 + em, el * el + el + em, el * el + el - em
-			f_lm[ el * el + el - em ] = pow(-1.0, -em) * ( flm_real[healpy_lm(el, em, L)] ).conjugate()
-			f_lm[ el * el + el + em ] = flm_real[healpy_lm(el, em, L)]
+			f_lm[ el * el + el - em ] = pow(-1.0, -em) * ( flm_hp[healpy_lm(el, em, L)] ).conjugate()
+			f_lm[ el * el + el + em ] = flm_hp[healpy_lm(el, em, L)]
 
 	s2let_transform_axisym_lm_wav_analysis(
 		<double complex*> np.PyArray_DATA(f_wav_lm),
@@ -90,14 +86,59 @@ def pys2let_transform_axisym_lm_wav_analysis(
 	free(scal_lm);
 	free(wav_lm);
 
-	f_scal_lm_real = np.empty([L*(L+1)/2,], dtype=complex)
-	f_wav_lm_real = np.empty([L*(L+1)/2, J-J_min+1], dtype=complex)
+	f_scal_lm_hp = np.empty([L*(L+1)/2,], dtype=complex)
+	f_wav_lm_hp = np.empty([L*(L+1)/2, J-J_min+1], dtype=complex)
 
 	for el from 0 <= el < L:
 		for em from 0 <= em <= el:
-			#print el, em, el * (el + 1)/2 + em, el * el + el + em, el * el + el - em
-			f_scal_lm_real[healpy_lm(el, em, L)] = f_scal_lm[ el * el + el + em ]
+			f_scal_lm_hp[healpy_lm(el, em, L)] = f_scal_lm[ el * el + el + em ]
 			for j from 0 <= j <= J-J_min:
-				f_wav_lm_real[healpy_lm(el, em, L), j] = f_wav_lm[ L*L*j + el * el + el + em ]
+				f_wav_lm_hp[healpy_lm(el, em, L), j] = f_wav_lm[ L*L*j + el * el + el + em ]
 
-	return f_wav_lm_real, f_scal_lm_real
+	return f_wav_lm_hp, f_scal_lm_hp
+
+
+def pys2let_transform_axisym_lm_wav_synthesis(
+	np.ndarray[double complex, ndim=2, mode="c"] f_wav_lm_hp not None, 
+	np.ndarray[double complex, ndim=1, mode="c"] f_scal_lm_hp not None, B, L, J_min):
+
+	cdef s2let_parameters_t parameters = {};
+	parameters.B = B;
+	parameters.L = L;
+	parameters.J_min = J_min;
+	J = s2let_j_max(&parameters);
+
+	cdef double *wav_lm, *scal_lm;
+	s2let_transform_axisym_lm_allocate_wav(&wav_lm, &scal_lm, &parameters);
+	s2let_transform_axisym_lm_wav(wav_lm, scal_lm, &parameters);
+
+	f_lm = np.empty([L * L,], dtype=complex)
+	f_scal_lm = np.empty([L * L,], dtype=complex)
+	f_wav_lm = np.empty([L * L * (J+1-J_min),], dtype=complex)
+
+	for el from 0 <= el < L:
+		for em from 0 <= em <= el:
+			f_scal_lm[ el * el + el - em ] = pow(-1.0, -em) * f_scal_lm_hp[healpy_lm(el, em, L)].conjugate()
+			f_scal_lm[ el * el + el + em ] = f_scal_lm_hp[healpy_lm(el, em, L)]
+			for j from 0 <= j <= J-J_min:
+				f_wav_lm[ L*L*j + el * el + el - em ] = pow(-1.0, -em) * f_wav_lm_hp[healpy_lm(el, em, L), j].conjugate()
+				f_wav_lm[ L*L*j + el * el + el + em ] = f_wav_lm_hp[healpy_lm(el, em, L), j]
+
+
+	s2let_transform_axisym_lm_wav_synthesis(
+		<double complex*> np.PyArray_DATA(f_lm),
+		<double complex*> np.PyArray_DATA(f_wav_lm),
+		<double complex*> np.PyArray_DATA(f_scal_lm),
+		wav_lm, scal_lm, &parameters
+	);
+
+	free(scal_lm);
+	free(wav_lm);
+
+	f_lm_hp = np.empty([L*(L+1)/2,], dtype=complex)
+
+	for el from 0 <= el < L:
+		for em from 0 <= em <= el:
+			f_lm_hp[healpy_lm(el, em, L)] = f_lm[ el * el + el + em ]
+
+	return f_lm_hp
--- src/main/python/pys2let_test_axisym_hpx.py
+++ src/main/python/pys2let_test_axisym_hpx.py
@@ -16,8 +16,11 @@ fname = '/Users/bl/Dropbox/Astrodata/SDSS/Fields/Planck_EBV_256rQ.fits'
 
 # Read healpix map and compute alms. 
 # f_lm has size L*(L+1)/2
-f = hp.read_map(fname)
-f_lm = hp.map2alm(f, lmax=L-1)
+f_ini = hp.read_map(fname) # Initial map
+f_lm = hp.map2alm(f_ini, lmax=L-1) # Its alms
+f = hp.alm2map(f_lm, nside=nside, lmax=L-1) # Band limited version
+
+hp.mollview(f)
 
 # Call pys2let and compute wavelet transform. Returns the harmonic coefficients of the wavelets.
 # f_scal_lm has size L*(L+1)/2
@@ -32,5 +35,10 @@ for j in range(J-J_min+1):
 	flm = f_wav_lm[:,j].ravel()
 	f_wav[:,j] = hp.alm2map(flm, nside=nside, lmax=L-1)
 	hp.mollview(f_wav[:,j])
+
+# Uses synthesis to reconstruct the input map.
+f_lm_rec = pys2let_transform_axisym_lm_wav_synthesis(f_wav_lm, f_scal_lm, B, L, J_min)
+f_rec = hp.alm2map(f_lm_rec, nside=nside, lmax=L-1)
+hp.mollview(f_rec)
 
 plt.show()
